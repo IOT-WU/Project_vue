d7289eaaf65d3638a5a48662aee4dffb0f121245
WuJunHaoQ <wujunhao511@outlook.com>
2022-02-22 14:55:01 +0800

    菜单

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -2,28 +2,52 @@
     <!-- 菜单 -->
     <div>
         <el-container>
-            <el-aside width="auto">
-                <el-menu :default-openeds="['1', '3']">
+            <el-aside width="250px">
+                <el-menu :default-openeds="['1']">
                     <el-sub-menu index="1">
-                        <template #title> 用户信息显示 </template>
+                        <template #title> 流程管理 </template>
                         <el-menu-item index="1-1" @click="Goto('')"
-                            >角色信息</el-menu-item
+                            >发起流程</el-menu-item
                         >
                         <el-menu-item index="1-2" @click="Goto('')"
-                            >用户信息</el-menu-item
+                            >占位符</el-menu-item
                         >
                     </el-sub-menu>
                     <el-sub-menu index="2">
-                        <template #title> 我的测试 </template>
+                        <template #title> 系统管理 </template>
                         <el-sub-menu index="2-1">
-                            <template #title> 测试 </template>
+                            <template #title> 菜单管理 </template>
                             <el-menu-item index="2-1-1" @click="Goto('')"
-                                >测试表单数据</el-menu-item
+                                >菜单权限管理</el-menu-item
                             >
                             <el-menu-item index="2-1-2" @click="Goto('')"
-                                >二维码测试</el-menu-item
+                                >占位符</el-menu-item
                             >
                         </el-sub-menu>
+                    </el-sub-menu>
+                    <el-sub-menu index="3">
+                        <template #title> 数据管理 </template>
+                        <el-menu-item index="3-1" @click="Goto('')"
+                            >年假管理</el-menu-item
+                        >
+                        <el-menu-item index="3-2" @click="Goto('')"
+                            >维护管理</el-menu-item
+                        >
+                    </el-sub-menu>
+                    <el-sub-menu index="4">
+                        <template #title> 文档管理 </template>
+                        <el-menu-item index="4-1" @click="Goto('')"
+                            >归档管理</el-menu-item
+                        >
+                        <el-menu-item index="4-2" @click="Goto('')"
+                            >占位符</el-menu-item
+                        >
+                    </el-sub-menu>
+                    <el-sub-menu index="5">
+                        <template #title> 报表管理 </template>
+                        <el-menu-item index="5-1" @click="Goto('')"
+                            >占位符</el-menu-item
+                        >
                     </el-sub-menu>
                 </el-menu>
             </el-aside>
